ed9ed30dac47c7acc71b980556fea80f5f901fef
odilovalisher17 <77061401+odilovalisher17@users.noreply.github.com>
2023-10-12 20:16:05 +0500

    4.4

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const userRouter = require("./controllers/userController");
 
 const app = express();
 
+// Add CORS middleware
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
