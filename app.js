bc208a9aa381c0cf569dd2574ea2b1a370e35f14
odilovalisher17 <77061401+odilovalisher17@users.noreply.github.com>
2023-10-14 18:03:31 +0500

    v4.8.1

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://intern-itransition-task4.netlify.app/"
+    "https://intern-itransition-task4.netlify.app"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
