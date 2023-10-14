ebb3a4402cd17e6e4c0bf15e2d1fda3585b7d170
odilovalisher17 <77061401+odilovalisher17@users.noreply.github.com>
2023-10-14 18:00:28 +0500

    v4.8

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 // Add CORS middleware
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://intern-itransition-task4.netlify.app/"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
